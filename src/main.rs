cab6e64bcd60793841b32b0b7093070f84f35820
Shuya Kinjo <a8109058@gmail.com>
2022-04-15 00:21:55 +0900

    Add example code to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use failure::Fallible;
+// use std::error::Error;
+// use failure::Fallible;
+
+use headless_chrome::Browser;
+
+fn main() -> Fallible<()> {
+    let browser = Browser::default()?;
+
+    let tab = browser.wait_for_initial_tab()?;
+
+    tab.navigate_to("https://www.wikipedia.org/")?;
+    tab.wait_for_element("input#searchInput")?.click()?;
+
+    tab.type_str("WebKit")?;
+    tab.press_key("Enter")?;
+    tab.wait_for_element("#firstHeading")?;
+
+    println!("{}", tab.get_url());
+
+    assert_eq!(true, tab.get_url().ends_with("WebKit"));
+
+    Ok(())
 }
